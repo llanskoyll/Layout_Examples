fe1ae61346e599ce08c5f760473af16b424a8d2e
llanskoyll <artem.maximov.2000@bk.ru>
2022-02-21 18:16:09 +0300

    commit layout

---
+++ prj_3/Assets/js/app.js
@@ -0,0 +1,18 @@
+/*function factorial(x) {
+    if (x < 0) {
+        throw new Error("Значение x меньше 0");
+    }
+    let f;
+    for(f = 1; x > 1; x--) {
+        f *= x;
+    }
+    return f;
+}
+try {
+    let n = Number(prompt("Введите положительное число", ""));
+    let f = factorial(n);
+    alert(n + "! = " + f);
+} catch (ex) {
+    alert(ex);
+} */
+
